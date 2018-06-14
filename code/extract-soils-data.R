69171be52d30e1e538a7760611595efab8ad602b
swood-ecology <stephenawood@gmail.com>
2018-06-14 07:43:03 -0400

    Included US SOC function

--- code/extract-soils-data.R
+++ code/extract-soils-data.R
@@ -7,7 +7,9 @@
 
 # LOAD PACKAGES
 library(tidyverse)  # For data manipulation
-library(sf)         # For spatial objects
+library(sf)         # For spatial points objects
+library(raster)     # For SoilGrids raster data
+
 
 # READ IN DATA
 site_data <- read_csv("data/site-data.csv")
@@ -15,8 +17,10 @@ countries <- rnaturalearth::ne_countries(scale = 110, type = "countries", contin
                             country = NULL, geounit = NULL, sovereignty = NULL,
                             returnclass = "sf")
 
+
 # CONVERT POINTS TO SPATIAL CLASS
 sp_sites <- st_as_sf(site_data, coords = c("long_dec","lat_dec"), crs=4326)
+
 
 # PLOT LOCATIONS
 plot(
@@ -29,6 +33,24 @@ plot(
   cex=0.5,
   col="blue",
   add=T
-  )
+)
 
-# 
+
+# EXTRACT SOIL PROPERTIES FOR THE US POINTS
+## Filter data set to US points
+### Do I need to filter out Puerto Rico?
+us_sites <- filter(site_data,site.country=="United States") %>%
+  st_as_sf(coords = c("long_dec", "lat_dec"),crs = 4326)
+
+## Extract SOC values
+soc <- raster("data/SoilGrids/100m/soc_M_sl4_100m.tif")   # What units?
+us_sites <- as(us_sites,"Spatial") %>% 
+  raster::extract(soc,.) %>%
+  cbind(us_sites)
+names(us_sites)[1] <- "SOC"
+
+
+# EXTRACT SOIL PROPERTIES FOR NON-US POINTS
+## Filter data set to non-US points
+non.us_sites <- filter(site_data,site.country!="United States") %>%
+  st_as_sf(coords = c("long_dec", "lat_dec"),crs = 4326)
